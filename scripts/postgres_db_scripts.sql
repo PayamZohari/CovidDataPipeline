1ff283dcd2d4be4a1b4ef3cf42b12b2b88727f19
payam <payamzohari80@gmail.com>
2025-05-02 17:24:31 +0000

    add indexes to postgres table

--- scripts/postgres_db_scripts.sql
+++ scripts/postgres_db_scripts.sql
@@ -11,3 +11,9 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.covid_cases
     OWNER to airflow;
+
+
+-- Create indexes for efficient querying
+CREATE INDEX idx_covid_cases_date ON public.covid_cases(date);
+CREATE INDEX idx_covid_cases_state ON public.covid_cases(state);
+CREATE INDEX idx_covid_cases_county ON public.covid_cases(county);
